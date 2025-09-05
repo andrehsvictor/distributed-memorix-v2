f9bd2e1d93c1e373b45cb568b09d4cdf3d5f06ab
André Victor <andrehsvictor@gmail.com>
2025-09-05 17:08:33 -0300

    feat: add Maven Wrapper and initial project structure with commons module

---
+++ commons/src/test/java/io/github/andrehsvictor/memorix/commons/CommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.andrehsvictor.memorix.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
