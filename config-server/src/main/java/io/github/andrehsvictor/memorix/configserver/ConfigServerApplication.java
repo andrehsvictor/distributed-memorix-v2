a50769893cfc8a884875608c8443bd3b583ee1e5
André Victor <andrehsvictor@gmail.com>
2025-09-06 07:30:15 -0300

    feat: add ConfigServerApplication class and remove test class

--- config-server/src/main/java/io/github/andrehsvictor/memorix/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/io/github/andrehsvictor/memorix/configserver/ConfigServerApplication.java
@@ -1,8 +1,10 @@
-package io.github.andrehsvictor.memorix.config_server;
+package io.github.andrehsvictor.memorix.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
