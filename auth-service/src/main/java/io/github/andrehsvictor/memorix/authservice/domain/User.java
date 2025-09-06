d085c8399f58970cf61a8224a778e3510a9bb1a3
André Victor <andrehsvictor@gmail.com>
2025-09-06 08:34:39 -0300

    feat: add User entity with fields and JPA annotations

---
+++ auth-service/src/main/java/io/github/andrehsvictor/memorix/authservice/domain/User.java
@@ -0,0 +1,63 @@
+package io.github.andrehsvictor.memorix.authservice.domain;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+@EqualsAndHashCode(of = "id")
+@ToString(exclude = { "password" })
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -1743654935352969339L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Builder.Default
+    private Boolean emailVerified = false;
+
+    @Column(name = "password_hash")
+    private String password;
+
+    private String pictureUrl;
+    private String bio;
+    private String locale;
+    private Instant suspendedUntil;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+
+}
