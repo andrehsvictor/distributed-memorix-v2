d80135fc7e06eb5415404db5bc31017ec54758cc
André Victor <andrehsvictor@gmail.com>
2025-09-12 17:30:40 -0300

    feat: initialize deck-service with Maven wrapper and basic configuration files

---
+++ deck-service/src/test/java/io/github/andrehsvictor/memorix/deck_service/DeckServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.andrehsvictor.memorix.deck_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeckServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
