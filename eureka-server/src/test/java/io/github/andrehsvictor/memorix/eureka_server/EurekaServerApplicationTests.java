48ffc61df19e034d95874491c27f415984766d3e
André Victor <andrehsvictor@gmail.com>
2025-09-12 16:56:03 -0300

    feat: add eureka server implementation with Maven wrapper and configuration files

---
+++ eureka-server/src/test/java/io/github/andrehsvictor/memorix/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.andrehsvictor.memorix.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
