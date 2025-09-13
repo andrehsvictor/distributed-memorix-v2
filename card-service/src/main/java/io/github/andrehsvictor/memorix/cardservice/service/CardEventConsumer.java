63347d16691795e3d70a949e4478dca787c9e775
André Victor <andrehsvictor@gmail.com>
2025-09-13 14:40:50 -0300

    feat: add DeckDeletedEvent class and update CardEventConsumer to handle deck deletion events

--- card-service/src/main/java/io/github/andrehsvictor/memorix/cardservice/service/CardEventConsumer.java
+++ card-service/src/main/java/io/github/andrehsvictor/memorix/cardservice/service/CardEventConsumer.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import io.github.andrehsvictor.memorix.cardservice.event.DeckDeletedEvent;
 import io.github.andrehsvictor.memorix.cardservice.repository.CardRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,9 +18,9 @@ public class CardEventConsumer {
     private final CardRepository cardRepository;
 
     @RabbitListener(queues = { "deck.deleted" })
-    public void handleDeckDeletedEvent(String deckId) {
-        long count = cardRepository.deleteAllByDeckId(UUID.fromString(deckId));
-        log.info("Deleted {} cards associated with deck ID {}", count, deckId);
+    public void handleDeckDeletedEvent(DeckDeletedEvent event) {
+        long count = cardRepository.deleteAllByDeckId(UUID.fromString(event.getDeckId()));
+        log.info("Deleted {} cards associated with deck ID {}", count, event.getDeckId());
     }
 
 }
