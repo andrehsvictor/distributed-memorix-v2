62e74e19c6f28ada51b33087d5d01c1ede949eae
André Victor <andrehsvictor@gmail.com>
2025-09-17 11:12:31 -0300

    feat: Add CORS configuration and JSON message converter for RabbitMQ in services

--- card-service/src/main/java/io/github/andrehsvictor/memorix/cardservice/config/RabbitConfig.java
+++ card-service/src/main/java/io/github/andrehsvictor/memorix/cardservice/config/RabbitConfig.java
@@ -7,6 +7,10 @@ import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -47,5 +51,17 @@ public class RabbitConfig {
     @Bean
     Binding bindingDeckDeletedDlq(Queue deckDeletedDlq, Exchange deckDlxExchange) {
         return BindingBuilder.bind(deckDeletedDlq).to(deckDlxExchange).with("deck.deleted.dlq").noargs();
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
     }
 }
