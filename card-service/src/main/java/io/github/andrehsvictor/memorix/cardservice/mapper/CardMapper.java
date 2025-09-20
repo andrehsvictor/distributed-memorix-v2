ca5d2381d751073c20b92cafa8fd35b53800bb52
André Victor <andrehsvictor@gmail.com>
2025-09-20 08:35:27 -0300

    refactor: Remove afterMapping method from CardMapper to simplify mapping logic

--- card-service/src/main/java/io/github/andrehsvictor/memorix/cardservice/mapper/CardMapper.java
+++ card-service/src/main/java/io/github/andrehsvictor/memorix/cardservice/mapper/CardMapper.java
@@ -1,11 +1,9 @@
 package io.github.andrehsvictor.memorix.cardservice.mapper;
 
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import io.github.andrehsvictor.memorix.cardservice.dto.DeckDto;
 import io.github.andrehsvictor.memorix.cardservice.dto.CardWithDeckDto;
 import io.github.andrehsvictor.memorix.cardservice.dto.PostCardDto;
 import io.github.andrehsvictor.memorix.cardservice.dto.PutCardDto;
@@ -23,11 +21,5 @@ public abstract class CardMapper {
     public abstract Card updateCardFromPutCardDto(PutCardDto putCardDto, @MappingTarget Card card);
 
     public abstract CardWithDeckDto cardToCardWithDeckDto(Card card);
-
-    @AfterMapping
-    protected void afterMapping(Card card, @MappingTarget CardWithDeckDto dto) {
-        DeckDto deckDto = deckService.getById(card.getDeckId());
-        dto.setDeck(deckDto);
-    }
 
 }
